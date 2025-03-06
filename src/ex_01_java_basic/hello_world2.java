7f8dbc88bee1e3b965d9571656a76cc5e4eb2aac
tushargithub23 <tushar.ambuskar7@gmail.com>
2025-03-06 08:39:25 +0530

    First_Program

---
+++ src/ex_01_java_basic/hello_world2.java
@@ -0,0 +1,8 @@
+package ex_01_java_basic;
+
+public class hello_world2 {
+    public static void main(String[] args)
+    {
+        System.out.println("Hi Tushar here welcomes you in java sessions");
+    }
+}
