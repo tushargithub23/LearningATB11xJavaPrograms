d845d3c66c3975d61addacf8e6bb71eb5cfb5e25
tushargithub23 <tushar.ambuskar7@gmail.com>
2025-03-06 08:44:52 +0530

    SelfGit Push

---
+++ src/ex_01_java_basic/TusharSelf.java
@@ -0,0 +1,13 @@
+package ex_01_java_basic;
+
+import java.sql.SQLOutput;
+
+public class TusharSelf
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Enjyoing First Session");
+        System.out.println("Pushing this 1st program to Git");
+
+    }
+}
