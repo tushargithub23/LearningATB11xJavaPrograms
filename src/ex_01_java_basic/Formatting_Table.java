19e49f2dd29e51f0f7cee9bb8c306ebdb648c012
tushargithub23 <tushar.ambuskar7@gmail.com>
2025-03-11 08:42:39 +0530

    Formatting_Table %d%n

---
+++ src/ex_01_java_basic/Formatting_Table.java
@@ -0,0 +1,25 @@
+package ex_01_java_basic;
+
+
+
+public class Formatting_Table {
+    public static void main(String[] args) {
+        System.out.println("==== Table 9 =======");
+        int no = 9;
+        System.out.println("Your Table no is "+no);
+
+        System.out.printf("9 * 1 = %d%n", 9 * 1);
+        System.out.printf("9 * 2 = %d%n", 9 * 2);
+        System.out.printf("9 * 3 = %d%n", 9 * 3);
+        System.out.printf("9 * 4 = %d%n", 9 * 4);
+        System.out.printf("9 * 5 = %d%n", 9 * 5);
+        System.out.printf("9 * 6 = %d%n", 9 * 6);
+        System.out.printf("9 * 7 = %d%n", 9 * 7);
+        System.out.printf("9 * 8 = %d%n", 9 * 8);
+        System.out.printf("9 * 9 = %d%n", 9 * 9);
+        System.out.printf("9 * 10 = %d%n", 9 * 10);
+    }
+}
+
+
+
