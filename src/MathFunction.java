5f68eb081351200512a2beaa4c8659650a1f2fe3
tushargithub23 <tushar.ambuskar7@gmail.com>
2025-03-12 23:37:59 +0530

    Task12March

---
+++ src/MathFunction.java
@@ -0,0 +1,14 @@
+
+public class MathFunction
+{
+    public static void main(String[] args)
+    {
+        System.out.println( "cuberoot of x2+y2-|z|");
+        int x=10;
+        int y=10;
+        int z=10;
+        double result;
+        result = Math.cbrt((Math.pow(x,2))+Math.pow(y,2)-Math.abs(z));
+        System.out.println("result of given equation is: "+result);
+    }
+}
