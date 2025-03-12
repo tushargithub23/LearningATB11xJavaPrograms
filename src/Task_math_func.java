dce6284f8c5f4b606bb6f01eb75d73265a29d6f3
tushargithub23 <tushar.ambuskar7@gmail.com>
2025-03-12 23:38:13 +0530

    Task12March

---
+++ src/Task_math_func.java
@@ -0,0 +1,25 @@
+public class Task_math_func
+{
+    public static void main(String[] args) {
+        System.out.println("Task-12-03-2025");
+        int x=15; int y=10;
+       //  Arithamtic Operators;
+        System.out.println(x+y);
+        System.out.println(x-y);
+        System.out.println(x*y);
+        System.out.println(x/y);
+        System.out.println(x%y);
+
+        //compound assignment operator
+        int age=50;
+        age+=10;
+        System.out.println("Age is: "+age);
+
+        age-=15;
+        System.out.println("Age is: "+age);
+
+        age/=5;
+        System.out.println("Age is: "+age);
+
+    }
+}
