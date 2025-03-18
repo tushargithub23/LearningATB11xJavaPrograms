70ba65365932315293c12ecd3cf100f5b7918b48
tushargithub23 <tushar.ambuskar7@gmail.com>
2025-03-18 15:22:13 +0530

    Commit

--- src/Practise_Task_programs/GitPractise.java
+++ src/Practise_Task_programs/GitPractise.java
@@ -1,4 +1,11 @@
 package Practise_Task_programs;
 
-public class GitPractise {
+public class GitPractise
+{
+    public static void main(String[] args) {
+        System.out.println("Tushar here ");
+        int a=10+20; int b=10;
+        int result=a+b;
+        System.out.println(result);
+    }
 }
